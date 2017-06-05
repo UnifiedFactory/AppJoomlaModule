93d55f55ce506462548add8744f1896be4558e45
pawel <pawel.dabrowski@unifiedfactory.com>
2017-06-05 13:25:42 +0200

    fix for JED checker

--- helper.php
+++ helper.php
@@ -1,8 +1,10 @@
 <?php
 
+defined('_JEXEC') or die;
+
 $db    = JFactory::getDbo();
 $query = $db->getQuery(true)
-	->select($db->quoteName('license'))
+	->select($db->quoteName('script'))
 	->from($db->quoteName('#__ufWidget'))
 	->where('lang = ' . $db->Quote('en-GB'));
 $db->setQuery($query);
